3f84f488cff8710aae45a5eceb25a6ead87af63c
Vlad Paulechka <vlad9.ru@gmail.com>
2023-07-11 11:19:39 +0300

    install commit

--- alembic/versions/7cd0ca4f4239_create_db.py
+++ alembic/versions/7cd0ca4f4239_create_db.py
@@ -24,13 +24,13 @@ def upgrade() -> None:
                     sa.PrimaryKeyConstraint('id')
                     )
 
-    # op.create_table('referrals',
-    #                 sa.Column('id', sa.SmallInteger(), nullable=False),
-    #                 sa.Column('user_id', sa.BigInteger(), nullable=False),
-    #                 sa.Column('referral_id', sa.BigInteger(), nullable=False),
-    #                 sa.ForeignKeyConstraint(['user_id'], ['user.id'], ondelete='NO ACTION'),
-    #                 sa.PrimaryKeyConstraint('id')
-    #                 )
+    op.create_table('referrals',
+                    sa.Column('id', sa.SmallInteger(), nullable=False),
+                    sa.Column('user_id', sa.BigInteger(), nullable=False),
+                    sa.Column('referral_id', sa.BigInteger(), nullable=False),
+                    sa.ForeignKeyConstraint(['user_id'], ['user.id'], ondelete='NO ACTION'),
+                    sa.PrimaryKeyConstraint('id')
+                    )
 
     op.create_table('users',
                     sa.Column('id', sa.BigInteger(), nullable=False),
@@ -46,3 +46,4 @@ def upgrade() -> None:
 def downgrade() -> None:
     op.drop_table('subscriptions')
     op.drop_table('users')
+    op.drop_table('referrals')
